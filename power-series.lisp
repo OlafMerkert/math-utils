189d36c143523f305c232601c43417a18f61c16e
Olaf Merkert <olaf@m-merkert.de>
2012-06-29 09:58:48 +0200

    creation helper for power series, simplification

--- power-series.lisp
+++ power-series.lisp
@@ -8,7 +8,7 @@
   ((degree :initform 0
            :initarg :degree
            :reader degree)
-   (coefficients :initform (la% 0 1)
+   (coefficients :initform (la% 0)
                  :initarg :coefficients
                  :reader coefficients)
    #|(var :initform 'x
@@ -34,3 +34,49 @@ pipe ends before."
       (nth-coefficient% series (- (degree series) n))
       0))
 
+(defun make-power-series/polynomial (leading-coefficient &rest coefficients)
+  (when (zerop leading-coefficient)
+    (error "Cannot define a power series [~A~{ ~A~}] with zero leading coefficient."
+           leading-coefficient coefficients))
+  (make-instance 'power-series
+                 :degree (length coefficients)
+                 :coefficients (apply #'la% 0 leading-coefficient coefficients)))
+
+(defmethod +-unit ((number power-series))
+  (make-instance 'power-series
+                 :degree 0
+                 :coefficients (la% 0)))
+
+(defmethod *-unit ((number power-series))
+  (make-power-series/polynomial 1))
+
+;; TODO visualising polynomials and power series
+
+(defparameter default-series-simplification-depth 100)
+
+(defmethod simplify ((series power-series) &key (depth default-series-simplification-depth))
+  "Remove zeroes from the start of SERIES and adjust the degree
+  accordingly.  In order to avoid infinite loops, at most
+  NORMALISATION-DEPTH entries are removed.  Thus the result need not
+  satisfy SERIES-NORMALISED-P.  This is indicated by the negative sign
+  of the second value, describing the necessary reduction in degree.
+  Additionally, when the series is marked finite, and
+  NORMALISATION-DEPTH is reached, we will assume the SERIES is 0."
+  (let* ((coeff (coefficients series))
+         (non-zero (loop
+                      for i from 0 below depth
+                      for zp = (gm:= 0 (lazy-aref coeff i))
+                      while zp
+                      finally (return (if zp (+ i 1) i)))))
+    (if (and (lazy-array-finite coeff)
+             (= non-zero depth))
+        ;; for finite series, treat reaching normalisation-depth as
+        ;; having found the 0 series.
+        (+-unit series)
+        (values
+         (make-instance 'power-series
+                        :degree (- (degree series) non-zero)
+                        :coefficients (lazy-array-drop coeff non-zero))
+         (if (= non-zero depth)
+             (- non-zero)
+             non-zero)))))
