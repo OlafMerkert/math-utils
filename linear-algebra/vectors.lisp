a424f206eafa2ae4e953897dc94c80a306c4dfd3
Olaf Merkert <olaf@m-merkert.de>
2012-07-25 12:51:51 +0200

    the mighty fill-array function to fill arrays efficiently with a function.

--- linear-algebra/vectors.lisp
+++ linear-algebra/vectors.lisp
@@ -41,3 +41,43 @@ list last)"
       (values nil (first list))))
 
 (defsetf mref set-mref)
+
+(defun make-vector% (dimensions fill-function)
+  "create a new array with given dimensions and use fill-function to
+  calculate the entries."
+  (declare (inline fill-array))
+  (let ((coeff (make-array dimensions :initial-element nil)))
+    (fill-array coeff fill-function dimensions)))
+
+(defun fill-array (array fill-function &optional (positions nil positions?))
+  "fill the ARRAY with the FILL-FUNCTION in the places given by
+POSITIONS. POSITIONS is a list of index ranges, where an index range
+is either a tuple (start end) with inclusive start and exclusive end,
+or simply an integer end, equivalent to (0 end). FILL-FUNCTION will be
+called in the same way as aref--first argument is the array, the
+remaining are the indices. "
+  (unless positions?
+    (setf positions (array-dimensions array)))
+  ;; just reverse the positions once here, so we don't have to reverse
+  ;; the index-lists all the time.
+  (setf positions (reverse positions))
+  (labels ((index-range (position)
+             ;; normalise the range information
+             (if (listp position)
+                 (values (first position) (second position))
+                 (values 0 position)))
+           (rec (positions indices)
+             (if positions
+                 ;; more ranges to iterate over
+                 (multiple-value-bind (start end) (index-range (first positions))
+                   (dotimes+ (i start end)
+                       ((rest (rest positions)))
+                     (rec rest
+                          (cons i indices))))
+                 ;; all index information available
+                 (setf (apply #'aref        array indices)
+                       (apply fill-function array indices)))))
+    (rec positions nil)
+    ;; return the now filled array
+    array))
+
