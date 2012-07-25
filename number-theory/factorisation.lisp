c742870c96ace10d5e916af6005a5804587746eb
Olaf Merkert <olaf@m-merkert.de>
2012-07-25 11:23:43 +0200

    fast factorisation of small integers

--- number-theory/factorisation.lisp
+++ number-theory/factorisation.lisp
@@ -1,8 +1,31 @@
 (defpackage :number-theory/factorisation
   (:nicknames :nt-f)
   (:use :cl :ol )
-  (:export))
+  (:export
+   :factorise))
 
 (in-package :number-theory/factorisation)
 
+(defun factorise% (n)
+  "Find all the prime factors of the given integer and return them in
+a list, sorted by size. Multiple factors appear multiple times."
+  (do ((i 2)
+       (m n)
+       (s (floor (sqrt n)))
+       (divs nil))
+      ((or (= m 1) (> i s))
+       (if (= m 1) (nreverse divs) (nreverse (cons m divs))))
+    (multiple-value-bind (mm rr) (floor m i)
+      (if (zerop rr)
+          (progn
+            (push i divs)
+            (setf m mm))
+          (incf i)))))
 
+(defun factorise (n &optional (compress t))
+  "As factorise%, but if compress is t, compress the list of
+  factors."
+  (ecase compress
+    ((nil) #1=(factorise% n))
+    ((t)  (compress #1#))
+    ((:singletons) (compress #1# :singletons t))))
