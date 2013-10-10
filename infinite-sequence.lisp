1963f86e1a0581e4b5bcefb571bf3f14a2162d58
Olaf Merkert <olaf@m-merkert.de>
2013-10-10 17:02:57 +0200

    add some documentation for infinite-sequences

--- infinite-sequence.lisp
+++ infinite-sequence.lisp
@@ -1,5 +1,6 @@
 (defpackage :infinite-sequence
   (:nicknames :infseq)
+  (:shadow #:length)
   (:use :cl :ol :iterate
         :infinite-math)
   (:export))
@@ -27,30 +28,53 @@
 ;;   (:documentation "An infinite sequence on integers, covering all
 ;;   integers"))
 
+(defmethod finite-sequence-p ((seq infinite+-sequence))
+  (numberp (length seq)))
+
+(defgeneric length (sequence))
+
+(defmethod length ((iseq infinite+-sequence))
+  (with-slots (start end) iseq
+    (gm:- end start)))
+
 (defmacro with-iseq (&body body)
+  "Bind slots `start' and `data+'. Moreover, transform the sequence
+index `n' to the array index `i'."
   `(with-slots (start data+) iseq
      (let ((i (- n start)))
        ,@body)))
 
 (defun ensure-array-size (array size)
-  (if (< (length array) size)
+  "For an adjustable `array', make sure that it can hold at least
+`size' elements."
+  (if (< (cl:length array) size)
       (adjust-array array (+ size 10) :initial-element +uncalculated+)
       array))
 
 (defun sref (iseq n)
-  (with-iseq 
+  "Access element at position `n' in a sequence. Compute previously
+uncalculated values."
+  (with-iseq
    (ensure-array-size data+ i)
    (let ((value (aref data+ i)))
      (if (eq value +uncalculated+)
          (compute-value iseq n)
          value))))
 
+;;; TODO provide different update strategies: sequential filling, as
+;;; needed and maybe others
+
+;;; TODO make the sequence interface compatible with arrays and lists.
+
 (defun compute-value (iseq n)
+  "Fill position `n' in the sequence `iseq' using the
+`generating-function'."
   (with-iseq 
    (setf (aref data+ i)
          (funcall (generating-function iseq) n))))
 
 (defun set-sref (iseq n value)
+  "Manually set position `n' in the sequence `iseq'."
   (with-iseq
     (ensure-array-size (data+ iseq) i)
     (setf (aref data+ i)
--- math-utils.asd
+++ math-utils.asd
@@ -2,6 +2,7 @@
   :depends-on (ol-utils cl-utilities iterate)
   :components ((:file "generic-math")
                (:file "infinite-math" :depends-on ("generic-math"))
+               (:file "infinite-sequence" :depends-on ("infinite-math"))
                (:file "elementary-combinatorics")
                (:module "number-theory"
                         :depends-on ("generic-math")
