646d62b3122e33ddc8205b110daf5d089afa5828
Olaf Merkert <olaf@m-merkert.de>
2013-10-12 19:43:10 +0200

    start porting power-series to infinite-sequence, with the obvious stuff

--- power-series.lisp
+++ power-series.lisp
@@ -4,7 +4,9 @@
   (:shadowing-import-from :generic-math :summing)
   (:use :cl :ol :generic-math
         :iterate
-        :polynomials)
+        :polynomials
+        :infinite-math
+        :infinite-sequence)
   (:export
    #:confidence
    #:nth-coefficient%
@@ -24,11 +26,16 @@
 
 (in-package :power-series)
 
+(declaim (inline finite-coefficients))
+(defun finite-coefficients (&rest coefficients)
+  (finseq (apply #'vector coefficients) 0))
+
+
 (defclass power-series (generic-math-object)
   ((degree :initform 0
            :initarg :degree
            :reader degree)
-   (coefficients :initform (la% 0)
+   (coefficients :initform (finite-coefficients)
                  :initarg :coefficients
                  :reader coefficients)
    (var :initform 'x
@@ -42,7 +49,7 @@
   coefficient in VAR^0."))
 
 (defun make-constant-series (constant)
-  (make-instance 'constant-series :coefficients (la% 0 constant)))
+  (make-instance 'constant-series :coefficients (finite-coefficients constant)))
 
 ;; TODO add support for different variables (also when working
 ;; together with polynomials).
@@ -50,38 +57,35 @@
 (defmethod simplified-p ((series power-series))
   "Test whether the first coefficient is indeed not 0, so the degree is
   meaningful."
-  (not (zero-p (lazy-aref (coefficients series) 0))))
+  (not (zero-p (sref (coefficients series) :start))))
 
 (defmethod simplified-p ((series constant-series))
   t)
 
 (defmethod leading-coefficient ((power-series power-series))
-  (if (simplified-p power-series)
-      (nth-coefficient% power-series 0)
-      (error "Trying to take leading-coefficient of non-simplified power series.")))
+  (let ((lead-coeff (nth-coefficient power-series :start)))
+    (if (not (zero-p lead-coeff))
+        lead-coeff
+        (error 'unsupported-operation-on-unsimplified))))
 
 (defmethod leading-coefficient ((series constant-series))
-    (nth-coefficient% series 0))
+    (nth-coefficient series 0))
 
 (defmethod nth-coefficient% ((series power-series) n)
   "Return the nth element of the coefficients pipe--or zero, if the
 pipe ends before."
-  (unless (>= n 0)
-    (error "Non negative index ~A in NTH-COEFFICIENT%." n))
-  (lazy-aref (coefficients series) n))
+  (error 'unsupported-operation))
 
 (defmethod nth-coefficient ((series power-series) n)
   "Return the coefficient of X^n"
-  (if (<= n (degree series))
-      (nth-coefficient% series (- (degree series) n))
-      0))
+  (sref (coefficients series) n))
 
 (defmethod -> ((target-type (eql 'power-series)) (polynomial polynomial) &key)
   (if (zerop (degree polynomial))
       (make-constant-series (constant-coefficient polynomial))
       (make-instance 'power-series
                      :degree (degree polynomial)
-                     :coefficients (la% 0 (coefficients polynomial)))))
+                     :coefficients (finseq (coefficients polynomial) 0))))
 
 (defmethod -> ((target-type power-series) (polynomial polynomial) &key)
   (-> 'power-series polynomial))
@@ -103,17 +107,14 @@ COEFFICIENTS."
            leading-coefficient coefficients))
   (make-instance 'power-series
                  :degree (or degree (length coefficients))
-                 :coefficients (apply #'la% 0 leading-coefficient coefficients)))
+                 :coefficients (apply #'finite-coefficients leading-coefficient coefficients)))
 
 (defmacro make-power-series/inf (degree formula)
   "Create a new power-series with given DEGREE and coefficients given
 by FORMULA where INDEX is anaphorically bound."
   `(make-instance 'power-series
                   :degree ,degree
-                  :coefficients (make-lazy-array (:index-var index :default-value 0)
-                                  ,formula)))
-
-;; TODO visualising polynomials and power series
+                  :coefficients (inf-seq nil (index) ,formula)))
 
 (defparameter default-series-simplification-depth 100)
 
@@ -126,18 +127,20 @@ by FORMULA where INDEX is anaphorically bound."
   Additionally, when the series is marked finite, and
   NORMALISATION-DEPTH is reached, we will assume the SERIES is 0."
   (let* ((coeff (coefficients series))
-         (non-zero (iter (for i from 0 below depth)
-                         (finding i such-that (not (zero-p (lazy-aref coeff i)))
-                                  on-failure depth))))
-    (if (and (lazy-array-finite coeff)
+         (non-zero (iter (for i from (degree series) above (- (degree series) depth))
+                         (finding i such-that (not (zero-p (sref coeff i)))
+                                  on-failure (- (degree series) depth)))))
+    (if (and (lazy-array-finite coeff) ;; todo new indices should
+             ;; simplify this, moreover we have better tests for
+             ;; finite stuff
              (= non-zero depth))
         ;; for finite series, treat reaching normalisation-depth as
         ;; having found the 0 series.
-        (zero series)
+        0
         (values
          (make-instance 'power-series
                         :degree (- (degree series) non-zero)
-                        :coefficients (lazy-array-drop coeff non-zero))
+                        :coefficients (subsequence coeff infinity- non-zero))
          (if (= non-zero depth)
              (- non-zero)
              non-zero)))))
@@ -146,6 +149,7 @@ by FORMULA where INDEX is anaphorically bound."
   (values series 0))
 
 (defmethod generic-* ((series-a power-series) (series-b power-series))
+  ;; todo adjust index arithmetic
   (let ((array-a (coefficients series-a))
         (array-b (coefficients series-b)))
     (make-instance 'power-series
@@ -158,12 +162,15 @@ by FORMULA where INDEX is anaphorically bound."
                                    :finite (la-finite-test (array-a array-b)
                                              (+ array-a array-b)))
                      (summing (i 0 n)
-                              (gm:* (lazy-aref array-a i)
-                                    (lazy-aref array-b (- n i))))))))
+                              (gm:* (sref array-a i)
+                                    (sref array-b (- n i))))))))
 
 (defmethod generic-* ((series-a constant-series) (series-b constant-series))
   (make-constant-series (generic-* (constant-coefficient series-a)
                                    (constant-coefficient series-b))))
+
+;;; todo maybe figure out a better system for dealing with constants.
+;;; (first take care of polynomials, perhaps?
 
 (defmethod generic-* ((series-a power-series) (series-b constant-series))
   (generic-* series-b series-a))
@@ -174,26 +181,25 @@ by FORMULA where INDEX is anaphorically bound."
     (make-instance 'power-series
                    :degree (degree series-b)
                    :coefficients
-                   (make-lazy-array (:index-var n :default-value 0
-                                                :finite (lazy-array-finite (coefficients series-b)))
-                                   (gm:* number (lazy-aref (coefficients series-b) n))))))
+                   (map-sequence (lambda (x) (gm:* number x)) (coefficients series-b)))))
 
 (defmethod generic-/ ((series-numer power-series) (series-denom power-series))
   (unless (simplified-p series-denom)
     (error "Cannot divide by the SERIES-DENOM ~A unless it is
     normalised, i.e. the first coefficient is non-zero." series-denom))
-  (let ((a0 (nth-coefficient% series-denom 0))
+  ;; todo adjust indices
+  (let ((a0 (leading-coefficient series-denom))
         (an (coefficients series-denom))
         (cn (coefficients series-numer)))
     (make-instance 'power-series
                    :degree (- (degree series-numer)
                               (degree series-denom))
-                   :coefficients (make-lazy-array (:start ((gm:/ (lazy-aref cn 0) a0))
+                   :coefficients (make-lazy-array (:start ((gm:/ (sref cn 0) a0))
                                                           :index-var n
                                                           :default-value 0)
-                                   (gm:/ (gm:- (lazy-aref cn n)
+                                   (gm:/ (gm:- (sref cn n)
                                                (summing (i 1 n)
-                                                        (gm:* (lazy-aref an i)
+                                                        (gm:* (sref an i)
                                                               (aref this (- n i)))))
                                          a0)))))
 
@@ -210,8 +216,9 @@ by FORMULA where INDEX is anaphorically bound."
   (unless (simplified-p series-denom)
     (error "Cannot invert the SERIES ~A unless it is properly
     normalised, i.e. first coefficient is non-zero." series-denom))
-  (let ((a0 (nth-coefficient% series-denom 0))
+  (let ((a0 (leading-coefficient series-denom))
         (an (coefficients series-denom)))
+    ;; todo adjust indices
     (make-instance 'power-series
                    :degree (- (degree series-denom))
                    :coefficients
@@ -219,7 +226,7 @@ by FORMULA where INDEX is anaphorically bound."
                                             :index-var n
                                             :default-value 0)
                      (gm:/ (gm:- (summing (i 1 n)
-                                          (gm:* (lazy-aref an i)
+                                          (gm:* (sref an i)
                                                 (aref this (- n i)))))
                            a0)))))
 
@@ -228,28 +235,22 @@ by FORMULA where INDEX is anaphorically bound."
 (defmethod generic-+ ((series-a power-series) (series-b power-series))
   "Add two series together.  Careful: This might destroy
   normalisation."
-  (if (> (degree series-a) (degree series-b))
-      (generic-+ series-b series-a)
-      ;; now series-b has the higher degree
-      (let ((coeff-a (coefficients series-a))
-            (coeff-b (coefficients series-b))
-            (d (- (degree series-b) (degree series-a))))
-        (make-instance 'power-series
-                       :degree (degree series-b)
-                       :coefficients
-                       (make-lazy-array (:index-var n :default-value 0
-                                                    :finite
-                                                    (la-finite-test (coeff-a coeff-b)
-                                                      (max (+ d coeff-a) coeff-b)))
-                         (if (< n d)
-                             (lazy-aref coeff-b n)
-                             (gm:+ (lazy-aref coeff-b n)
-                                   (lazy-aref coeff-a (- n d)))))))))
+  (let ((coeff-a (coefficients series-a))
+        (coeff-b (coefficients series-b)))
+    (make-instance 'power-series
+                   :degree (max (degree series-a) (degree series-b))
+                   :coefficients
+                   (map-sequences/or #'gm:+ coeff-a coeff-b))))
 
 (defmethod generic-- ((series-a power-series) (series-b power-series))
-  (generic-+ series-a
-             (generic-* (make-constant-series -1)
-                        series-b)))
+  "Add two series together.  Careful: This might destroy
+  normalisation."
+  (let ((coeff-a (coefficients series-a))
+        (coeff-b (coefficients series-b)))
+    (make-instance 'power-series
+                   :degree (max (degree series-a) (degree series-b))
+                   :coefficients
+                   (map-sequences/or #'gm:- coeff-a coeff-b))))
 
 (defmethod generic-+ ((series-a constant-series) (series-b constant-series))
   (make-constant-series (generic-+ (constant-coefficient series-a)
@@ -265,11 +266,12 @@ by FORMULA where INDEX is anaphorically bound."
   (let ((a0 (gm:sqrt (nth-coefficient% series 0)))
         (b  (coefficients series)))
     (make-instance 'power-series
-     :degree (/ (degree series) 2)
+                   :degree (/ (degree series) 2)
+                   ;; todo adjust indices
      :coefficients (make-lazy-array (:start (a0)
                                             :index-var n
                                             :default-value 0)
-                     (gm:/ (gm:- (lazy-aref b n)
+                     (gm:/ (gm:- (sref b n)
                                  (summing (i 1 n t) (gm:* (aref this i)
                                                           (aref this (- n i)))))
                            a0 2)))))
@@ -301,8 +303,8 @@ match, consider the series equal."
           (co-2 (coefficients series-2)))
       (when
           (iter (for i from 0 to confidence)
-                (always (gm:= (lazy-aref co-1 i)
-                              (lazy-aref co-2 i))))
+                (always (gm:= (sref co-1 i)
+                              (sref co-2 i))))
         confidence))))
 ;; TODO fix problems with possibly not yet simplified series (for
 ;; instance a series representing 0, but without "knowing" it)
@@ -316,12 +318,12 @@ match, consider the series equal."
   ;; Evaluate (all) the coefficients of polynomial parts
   (let ((d (degree series)))
    (if (minusp d)
-       (zero 'polynomial)
+       0
        (make-instance 'polynomial
                       ;; :degree d
-                      :coefficients (lazy-array-take (coefficients series)
-                                                     (+ d 1)
-                                                     nil)))))
+                      ;; todo check the array direction is correct
+                      :coefficients (seq->array (subsequence (coefficients series)
+                                                  0 infinity+))))))
 
 (defmethod series-truncate ((series constant-series))
   (make-polynomial (constant-coefficient series)))
@@ -335,7 +337,8 @@ match, consider the series equal."
         series ; no polynomial part -> nothing to do
         (make-instance 'power-series
                        :degree -1 
-                       :coefficients (lazy-array-drop (coefficients series) (+ d 1))))))
+                       :coefficients (subsequence (coefficients series)
+                                                  infinity- -1)))))
 
 (defmethod series-remainder ((series constant-series))
   (zero series))
@@ -346,7 +349,7 @@ match, consider the series equal."
    (make-instance 'power-series
                   :degree (degree power-series)
                   :coefficients
-                  (lazy-array-map
+                  (map-sequence
                    (lambda (x) (-> 'finite-fields:integer-mod x :mod mod))
                    (coefficients power-series)))))
 
