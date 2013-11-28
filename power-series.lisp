b57dbf8c9f30891c963e3c28820dcb75f33c8be3
Olaf Merkert <olaf@m-merkert.de>
2013-11-28 18:58:13 +0100

    go back to backing sequences for power-series starting from 0, going to infinity+
    * this makes addition and subtraction slightly more complicated,
    * but it makes division and multiplication actually tractable
    * also this should allow for some optimisation, we can build infinite
      sequences that always start at 0

--- power-series.lisp
+++ power-series.lisp
@@ -28,10 +28,18 @@
 
 (in-package :power-series)
 
+;; develop power-series backed by infinite sequences. In order to
+;; simplify the code, we always use sequences starting from 0, going
+;; up to infinity+. As a first step, implement it using
+;; infinite+-sequence with general starting bound, but perhaps we
+;; shall move to a slightly simpler datastructure for efficiency
+;; reasons (there might be a number of redundant operations
+;; happening). But first we want functionality.
+
 (declaim (inline finite-coefficients))
 (defun finite-coefficients (&rest coefficients)
   ;; todo maybe the reverse operation should go into an iseq function??
-  (seq->iseq/sv (reverse coefficients) :start 0 :end (- (length coefficients) 1) :standard-value 0))
+  (seq->iseq/sv coefficients :start 0 :end (- (length coefficients) 1) :standard-value 0))
 
 
 (defclass power-series (generic-math-object)
@@ -66,7 +74,7 @@
   t)
 
 (defmethod leading-coefficient ((power-series power-series))
-  (let ((lead-coeff (nth-coefficient power-series :end)))
+  (let ((lead-coeff (sref (coefficients  power-series) 0)))
     (if (not (zero-p lead-coeff))
         lead-coeff
         (error 'unsupported-operation-on-unsimplified))))
@@ -81,13 +89,14 @@ pipe ends before."
 
 (defmethod nth-coefficient ((series power-series) n)
   "Return the coefficient of X^n"
-  (sref (coefficients series) n))
+  (sref (coefficients series) (- (degree series) n)))
 
 (defmethod -> ((target-type (eql 'power-series)) (polynomial polynomial) &key)
   (if (zerop (degree polynomial))
       (make-constant-series (constant-coefficient polynomial))
       (make-instance 'power-series
                      :degree (degree polynomial)
+                     ;; todo check the right ordering of coefficients
                      :coefficients (seq->iseq/sv (coefficients polynomial) :start 0 :end (degree polynomial) :standard-value 0))))
 
 (defmethod -> ((target-type power-series) (polynomial polynomial) &key)
@@ -105,9 +114,7 @@ pipe ends before."
 `leading-coefficient' is zero."
   (make-instance 'power-series
                  :degree (or degree (- (length coefficients) 1))
-                 :coefficients (seq->iseq/sv (reverse (cons leading-coefficient coefficients))
-                                             :end degree :start infinite-math:infinity-
-                                             :standard-value 0)))
+                 :coefficients (apply #'finite-coefficients leading-coefficient coefficients)))
 
 (defun make-power-series (degree leading-coefficient &rest coefficients)
   "Create a new power-series with finitely many non-zero `coefficients'.
@@ -120,12 +127,14 @@ number of `coefficients'."
       (apply #'make-power-series% degree leading-coefficient coefficients)))
 
 
-(defmacro make-power-series/inf (degree formula)
+(defmacro! make-power-series/inf (o!degree formula)
   "Create a new power-series with given DEGREE and coefficients given
 by FORMULA where INDEX is anaphorically bound."
   `(make-instance 'power-series
-                  :degree ,degree
-                  :coefficients (inf-seq nil (index ,degree) ,formula)))
+                  :degree ,g!degree
+                  :coefficients (inf+seq nil (,g!index)
+                                  (let ((index (- ,g!degree ,g!index)))
+                                    ,formula))))
 
 (defparameter default-series-simplification-depth 100)
 
@@ -141,7 +150,8 @@ by FORMULA where INDEX is anaphorically bound."
   ;; simplification there)
   (with-slots (degree coefficients) series
     (mvbind (coeff stripped) (strip-if #'zero-p coefficients :from :end :limit depth)
-      (setf coefficients coeff
+      ;; todo perhaps integrate shifting into strip-if??
+      (setf coefficients (shift coeff (- stripped))
             degree (- degree stripped))
       (values series (if (>= stripped depth) (- stripped) stripped)))))
 
@@ -151,24 +161,14 @@ by FORMULA where INDEX is anaphorically bound."
 ;; todo how about series with finitely many entries
 (defmethod generic-* ((series-a power-series) (series-b power-series))
   (let* ((seq-a (coefficients series-a))
-         (seq-b (coefficients series-b))
-         (deg-a (degree series-a))
-         (deg-b (degree series-b))
-         (degree (+ deg-a deg-b)))
+         (seq-b (coefficients series-b)))
     (make-instance
      'power-series
-     :degree degree
+     :degree (+ (degree series-a) (degree series-b))
      ;; todo special case for finite sequences
-     :coefficients
-     (make-instance
-      'infinite--sequence
-      :end degree
-      :fill-strategy :sequential
-      :generating-function
-      (ilambda (this n)
-        (gm-summing (i deg-a (- n deg-b))
-                    (gm:* (sref seq-a i)
-                          (sref seq-b (- n i)))))))))
+     :coefficients (inf+seq nil (n)
+                     (gm-summing (i 0 n)
+                                 (gm:* (sref seq-a i) (sref seq-b (- n i))))))))
 
 (defmethod generic-* ((series-a constant-series) (series-b constant-series))
   (make-constant-series (generic-* (constant-coefficient series-a)
@@ -177,6 +177,7 @@ by FORMULA where INDEX is anaphorically bound."
 ;;; todo maybe figure out a better system for dealing with constants.
 ;;; (first take care of polynomials, perhaps?
 
+;; todo use a macro
 (defmethod generic-* ((series-a power-series) (series-b constant-series))
   (generic-* series-b series-a))
 
@@ -192,28 +193,19 @@ by FORMULA where INDEX is anaphorically bound."
   (unless (simplified-p series-denom)
     (error "Cannot divide by the SERIES-DENOM ~A unless it is
     normalised, i.e. the first coefficient is non-zero." series-denom))
-  ;; todo adjust indices
   (let ((a0 (leading-coefficient series-denom))
-        (deg-a (degree series-denom))
         (an (coefficients series-denom))
-        (cn (coefficients series-numer))
-        (degree (- (degree series-numer)
-                   (degree series-denom))))
+        (cn (coefficients series-numer)))
     (make-instance
      'power-series
-     :degree degree
+     :degree  (- (degree series-numer) (degree series-denom))
      :coefficients
-     (make-instance
-      'infinite--sequence
-      :data (vector (gm:/ (sref cn 0) a0))
-      :fill-strategy :sequential
-      :generating-function
-      (flambda (this n)
-        (gm:/ (gm:- (sref cn n)
-                    (gm-summing (i (- n 1) deg-a)
-                                (gm:* (sref an i)
-                                      (this (- n i)))))
-              a0))))))
+     (inf+seq (vector (gm:/ (sref cn 0) a0))
+         (n) (gm:/ (gm:- (sref cn n)
+                         (gm-summing (i 0 n t)
+                                     (gm:* (sref an (- n i))
+                                           (this i))))
+                   a0)))))
 
 (defmethod generic-/ ((series-numer constant-series) (series-denom constant-series))
   (make-constant-series (generic-/ (constant-coefficient series-numer)
@@ -228,50 +220,44 @@ by FORMULA where INDEX is anaphorically bound."
   (unless (simplified-p series-denom)
     (error "Cannot invert the SERIES ~A unless it is properly
     normalised, i.e. first coefficient is non-zero." series-denom))
-  (let ((a0 (leading-coefficient series-denom))
-        (deg-a (degree series-denom))
-        (an (coefficients series-denom)))
-    ;; todo adjust indices
+  (let* ((a0 (leading-coefficient series-denom))
+         (a0m (gm:- a0))
+         (an (coefficients series-denom)))
     (make-instance
      'power-series
      :degree (- (degree series-denom))
      :coefficients
-     (make-instance
-      'infinite--sequence
-      :data (vector (gm:/ (constant-coefficient series-numer) a0))
-      :fill-strategy :sequential
-      :generating-function
-      (flambda (this n)
-        (gm:/ (gm:- (gm-summing (i (- n 1) deg-a)
-                                (gm:* (sref an i)
-                                      (this (- n i)))))
-              a0))))))
+     (inf+seq (vector (gm:/ (constant-coefficient series-numer) a0))
+         (n)
+       (gm:/ (gm-summing (i 0 n t)
+                         (gm:* (sref an (- n i))
+                               (this i)))
+             a0m)))))
 
 ;; TODO perhaps consider additional simplification for units
 
-(defmethod generic-+ ((series-a power-series) (series-b power-series))
-  "Add two series together.  Careful: This might destroy
-  normalisation."
+(bind-multi ((generic-+ generic-+ generic--)
+             (gm:+ gm:+ gm:-)
+             (+uncalculated+ +uncalculated+ 0))
+ (defmethod generic-+ ((series-a power-series) (series-b power-series))
+  ;; Careful: This might destroy normalisation.
   (let ((coeff-a (coefficients series-a))
-        (coeff-b (coefficients series-b)))
+        (coeff-b (coefficients series-b))
+        (deg-a (degree series-a))
+        (deg-b (degree series-b)))
+    ;; in case the degrees are different, we need to offset one of the series
+    (cond ((< deg-a deg-b)
+           (setf coeff-a (shift coeff-a (- deg-b deg-a))))
+          ((< deg-b deg-a)
+           (setf coeff-b (shift coeff-b (- deg-a deg-b)))))
     (make-instance 'power-series
                    :degree (max (degree series-a) (degree series-b))
                    :coefficients
                    (map-sequences/or #'gm:+ +uncalculated+ coeff-a coeff-b))))
 
-(defmethod generic-- ((series-a power-series) (series-b power-series))
-  "Add two series together.  Careful: This might destroy
-  normalisation."
-  (let ((coeff-a (coefficients series-a))
-        (coeff-b (coefficients series-b)))
-    (make-instance 'power-series
-                   :degree (max (degree series-a) (degree series-b))
-                   :coefficients
-                   (map-sequences/or #'gm:- 0 coeff-a coeff-b))))
-
-(defmethod generic-+ ((series-a constant-series) (series-b constant-series))
-  (make-constant-series (generic-+ (constant-coefficient series-a)
-                                   (constant-coefficient series-b))))
+ (defmethod generic-+ ((series-a constant-series) (series-b constant-series))
+   (make-constant-series (generic-+ (constant-coefficient series-a)
+                                    (constant-coefficient series-b)))))
 
 (defmethod gm:sqrt ((series power-series))
   "Calculate a square root of this series--as long as the degree is
@@ -280,24 +266,21 @@ by FORMULA where INDEX is anaphorically bound."
                (evenp (degree series)))
     (error "Cannot take the root of SERIES ~A unless the degree is known to be even!" series))
   ;; now we essentially reduce to the case degree = 0
-  (let ((a0 (gm:sqrt (nth-coefficient% series 0)))
-        (b  (coefficients series))
-        (degree (/ (degree series) 2)))
+  (let* ((a0 (gm:sqrt (leading-coefficient series)))
+         (a0d (gm:* 2 a0))
+         (b  (coefficients series))
+         (degree (/ (degree series) 2)))
     (make-instance
      'power-series
      :degree degree
      ;; todo adjust indices
      :coefficients
-     (make-instance
-      'infinite--sequence
-      :data (vector a0)
-      :fill-strategy :sequential
-      :generating-function
-      (flambda (this n)
-        (gm:/ (gm:- (sref b n)
-                    (gm-summing (i 1 n t) (gm:* (this i)
-                                                (this (- n i)))))
-              a0 2))))))
+     (inf+seq  (vector a0)
+         (n)
+       (gm:/ (gm:- (sref b n)
+                   (gm-summing (i 1 n t) (gm:* (this i)
+                                               (this (- n i)))))
+             a0d)))))
 
 (defmethod gm:sqrt ((series constant-series))
   (multiple-value-bind (root nice) (gm:sqrt (constant-coefficient series))
@@ -324,29 +307,27 @@ they match, consider the series equal."
            (degree series-2))
     (let ((co-1 (coefficients series-1))
           (co-2 (coefficients series-2)))
-      (when
-          (iter (for i from 1 to confidence)
-                (for j downfrom (degree series-1))
-                (always (gm:= (sref co-1 j) (sref co-2 j))))
+      (when (iter (for i from 0 below confidence)
+                  (always (gm:= (sref co-1 i) (sref co-2 i))))
         confidence))))
 ;; TODO fix problems with possibly not yet simplified series (for
 ;; instance a series representing 0, but without "knowing" it)
 
 (defmethod generic-= ((series-1 constant-series) (series-2 constant-series))
-  (= (constant-coefficient series-1) (constant-coefficient series-2)))
+  (gm:= (constant-coefficient series-1) (constant-coefficient series-2)))
 
 ;; extracting and removing polynomial part of the laurent series
 (defmethod series-truncate ((series power-series))
   "Take the polynomial part of the given SERIES."
   ;; Evaluate (all) the coefficients of polynomial parts
   (let ((d (degree series)))
-   (if (minusp d)
-       0
-       (make-instance 'polynomial
-                      ;; :degree d
-                      ;; todo check the array direction is correct
-                      :coefficients (seq->array (subsequence (coefficients series)
-                                                  0 infinity+))))))
+    (if (minusp d)
+        0
+        (make-instance 'polynomial
+                       ;; :degree d
+                       ;; todo check the array direction is correct
+                       :coefficients (seq->array (subsequence (coefficients series)
+                                                              0 d))))))
 
 (defmethod series-truncate ((series constant-series))
   (make-polynomial (constant-coefficient series)))
@@ -361,7 +342,7 @@ they match, consider the series equal."
         (make-instance 'power-series
                        :degree -1 
                        :coefficients (subsequence (coefficients series)
-                                                  infinity- -1)))))
+                                                  (+ 1 d) infinity+)))))
 
 (defmethod series-remainder ((series constant-series))
   (zero series))
